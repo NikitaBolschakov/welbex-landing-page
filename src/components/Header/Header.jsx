5183edb7a119c3bf435466b892fe76ddcfb78865
Nikita Bolshakov <itbolshakov@yandex.ru>
2023-05-24 21:26:03 +0300

    feat: begining media 1440

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,38 @@ import styles from './Header.module.scss'
 function Header(props) {
     return (
         <div className={styles.header}>
+            <div className={styles.logo_wrapper}>
+                <div className={styles.logo}></div>
+                <caption className={styles.logo_caption}>крупный интегратор CRM в Росcии и ещё 8 странах</caption>
+            </div>
             <ul className={styles.list}>
-                <li className={styles.item}><a href="foo">услуги</a></li>
-                <li className={styles.item}><a href="foo">виджеты</a></li>
-                <li className={styles.item}><a href="foo">интеграции</a></li>
-                <li className={styles.item}><a href="foo">кейсы</a></li>
+                <li className={styles.item}><a href="foo">Услуги</a></li>
+                <li className={styles.item}><a href="foo">Виджеты</a></li>
+                <li className={styles.item}><a href="foo">Интеграции</a></li>
+                <li className={styles.item}><a href="foo">Кейсы</a></li>
             </ul>
+            <div className={styles.contact_wrapper}>
+            <a className={styles.number} href="tel:+7 555 555-55-55">
+                +7 555 555-55-55
+            </a>
+            <ul className={styles.list_networks}>
+                <li>
+                  <a className={styles.link} href="foo">
+                    <div className={styles.telegram}></div>
+                  </a>
+                </li>
+                <li>
+                  <a className={styles.link} href="foo">
+                    <div className={styles.tel}></div>
+                  </a>
+                </li>
+                <li>
+                  <a className={styles.link} href="foo">
+                    <div className={styles.wa}></div>
+                  </a>
+                </li>
+              </ul>
+            </div>
         </div>
     );
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,22 +5,48 @@ function Main(props) {
   return (
     <>
       <div className={styles.main}>
-        <h1 className={styles.title}>
-          Зарабатывайте больше <br />
-          <span>с&nbsp;WELBEX</span>
-        </h1>
-        <h2 className={styles.subtitle}>
-          Развиваем и контролируем продажи за вас
-        </h2>
-        <h3 className={styles.list_title}>
-          Вместе с&nbsp;<span>бесплатной консультацией</span> мы&nbsp;дарим:
-        </h3>
-        <ul className={styles.list}>
-          <li className={styles.item}><span className={styles.item_text}>Skype аудит</span></li>
-          <li className={styles.item}><span className={styles.item_text}>30 виджетов</span></li>
-          <li className={styles.item}><span className={styles.item_text}>Dashboard</span></li>
-          <li className={styles.item}><span className={styles.item_text}>Месяц аmoCRM</span></li>
-        </ul>
+        <div className={styles.lead}>
+          <h1 className={styles.title}>
+            Зарабатывайте больше <br />
+            <span>с&nbsp;WELBEX</span>
+          </h1>
+          <h2 className={styles.subtitle}>
+            Развиваем и контролируем продажи за вас
+          </h2>
+        </div>
+
+        <div className={styles.main_cards}>
+          <h3 className={styles.list_title}>
+            Вместе с&nbsp;<span>бесплатной консультацией</span> мы&nbsp;дарим:
+          </h3>
+          <ul className={styles.list}>
+            <li className={styles.item}>
+              <div className={styles.card}>
+                <p className={styles.card_title}>Виджеты</p>
+                <p className={styles.card_text}>30 готовых решений</p>
+              </div>
+            </li>
+            <li className={styles.item}>
+              <div className={styles.card}>
+                <p className={styles.card_title}>Dashboard</p>
+                <p className={styles.card_text}>с показателями вашего бизнеса</p>
+              </div>
+            </li>
+            <li className={styles.item}>
+              <div className={styles.card}>
+                <p className={styles.card_title}>Skype аудит</p>
+                <p className={styles.card_text}>отдела продаж и CRM системы</p>
+              </div>
+            </li>
+            <li className={styles.item}>
+              <div className={styles.card}>
+                <p className={styles.card_title}>35 дней</p>
+                <p className={styles.card_text}>использования CRM</p>
+              </div>
+            </li>
+          </ul>
+          <button className={styles.button}>Получить консультацию</button>
+        </div>
       </div>
       <div className={styles.overlay}>
         <div className={styles.glass}></div>
